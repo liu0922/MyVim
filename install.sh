78085064fa731145039e0c0c98924d117157d1b0
wenchang <wen@wen-U-100.(none)>
2015-01-02 04:16:51 -0500

    v2

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 rm -rf ~/.vim
-rm .vimrc
-sudo apt-get install  exuberant-ctags  cscope
+rm ~/.vimrc
+sudo rm -rf /usr/share/fonts/truetype/powerline-fonts
 
 mkdir powerline-fonts
 git clone https://github.com/Lokaltog/powerline-fonts ./powerline-fonts
 sudo mv powerline-fonts /usr/share/fonts/truetype/powerline-fonts
 sudo fc-cache -v -f
 
-git clone https://github.com/MvVim.git 
+mv .vim ~/.vim
+mv .vimrc ~/.vimrc
+
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/moll/vim-bbye.git ~/.vim/bundle/bbye
 
-mv .vim ~/.vim
-mv .vimrc ~/.vimrc
+sudo apt-get install  exuberant-ctags  cscope
